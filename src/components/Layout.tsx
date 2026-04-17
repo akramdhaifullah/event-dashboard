18f73ed6311ba67ca567707370dfa5fe2a5d0adf
Akram Dhaifullah <akramdhaifullah@webmail.umm.ac.id>
2026-04-17 12:41:33 +0700

    Remove admin's menu toggle

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <AppSidebar />
         <div className="flex-1 flex flex-col min-w-0">
           <header className="h-12 flex items-center border-b px-4 shrink-0 bg-background">
-            <SidebarTrigger />
+            <div className="md:hidden">
+              <SidebarTrigger />
+            </div>
           </header>
           <main className="flex-1 p-6 overflow-auto">{children}</main>
         </div>
